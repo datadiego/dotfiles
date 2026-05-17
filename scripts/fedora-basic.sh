76375cfd3198e784784c49c70c4d96776795f5bb
Juan Diego Mariscal Millan <juandiegomariscal@gmail.com>
2026-05-17 09:21:16 +0200

    update

--- scripts/fedora-basic.sh
+++ scripts/fedora-basic.sh
@@ -9,7 +9,7 @@ git config --global user.email "$git_email"
 sudo dnf update -y
 
 # utilidades
-sudo dnf install pipx unzip xclip yq jq fzf stow -y
+sudo dnf install pipx unzip xclip yq jq fzf stow starship -y
 
 # zoxide
 curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
